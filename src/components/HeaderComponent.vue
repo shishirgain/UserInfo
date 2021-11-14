fc676c3f8e53d8989aa9c4aae64adc91f8a92d40
Shishir <shishir0019@gmail.com>
2021-11-14 11:44:33 +0600

    init

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <headr>
+        Header
+        <hr>
+        {{ data }}
+    </headr>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        data: {
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navigator.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav>
+        <ul class="t">
+            <li v-for="(nav, index) in list" :key="`nav-${index}`"><a :href="Nav.url">{{ Nav.title }}</a> </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'navigator',
+    props: {
+        list: {
+            required: true,
+            default: []
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
